96540bd24d71b3d227b9d075ff179986cd8e08af
Philipp Stadler <hello@phstadler.com>
2016-11-29 22:39:15 +0100

    Added hardcoded instantiations

--- source/pils/layout.d
+++ source/pils/layout.d
@@ -13,25 +13,17 @@ class Layout
 public:
     @property Entity[] entities()
     {
-        return [];
-        version(none) {
-            return [
-                new Entity(
-                    "IkeaChairFusion",
-                    "/Users/phil/Dropbox/IM16/PRO1 libpils/Modelle/Sitzgruppe/IKEA_chair_FUSION/3cd4399871fb1a8768d1b9a1d97e2846_obj0_object.obj",
-                    vec3d(5.0, 2.0, 0.0),
-                    vec3d(0.01, 0.01, 0.01),
-                    quatd.identity()
-                ),
-                new Entity(
-                    "IkeaDeskLeksvik",
-                    "/Users/phil/Dropbox/IM16/PRO1 libpils/Modelle/Tisch/Schreibtisch--IKEA_desk_LEKSVIK/e4265ddbf415ef1877fe0205d04e5011_obj0_object.obj",
-                    vec3d(-5.0, 0.0, 0.0),
-                    vec3d(0.01, 0.01, 0.01),
-                    quatd.identity()
-                )
-            ];
-        }
+        auto lib = new EntityLibrary("examples/classlibs/krachzack");
+
+        EntityPrototype tofteryd = lib.findByID("krachzack.tofteryd");
+        Entity[] ents = [
+            tofteryd.instantiate(vec3d(-0.5, 0, 1), vec3d(1, 1, 1), quatd.identity()),
+            tofteryd.instantiate(vec3d(1, 0, 1), vec3d(1, 1, 1), quatd.identity())
+        ];
+
+        writeln(ents.toJSON.toString);
+
+        return ents;
     }
 
     @property string json()
